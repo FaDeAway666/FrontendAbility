fed77ae2e4f4328f99c444d3772730f91b7b6326
wangxi <136365575@qq.com>
2020-11-28 10:39:00 +0800

    FP Functor：add task Functor

--- js/函数式编程/Functor.js
+++ js/函数式编程/Functor.js
@@ -107,14 +107,41 @@ class IO {
 // 当调用的时候才会知道纯还是不纯，延迟执行不纯的操作
 // console.log(f.value())
 
-const { compose, curry} = require('folktale/core/lambda')
-const { toUpper, first} = require('lodash/fp')
+// folktale
+// const { compose, curry} = require('folktale/core/lambda')
+// const { toUpper, first} = require('lodash/fp')
 
-let f = curry(2, function(x,y) {
-    return x + y
-})
-console.log(f(1,2))
-console.log(f(1)(2))
+// let f = curry(2, function(x,y) {
+//     return x + y
+// })
+// console.log(f(1,2))
+// console.log(f(1)(2))
 
-let f2 = compose(toUpper, first);
-console.log(f2(['xixi','sisi']))
+// let f2 = compose(toUpper, first);
+// console.log(f2(['xixi','sisi']))
+
+//task 函子
+const { task } = require('folktale/concurrency/task')
+const fs = require('fs')
+function readFile(filename) {
+    return task(resolver => {
+        fs.readFile(filename, 'utf-8', function(err, data) {
+            if(err) resolver.reject(err)
+
+            resolver.resolve(data)
+        })
+    })
+}
+
+readFile('package.json')
+    .map(fp.split('\n'))
+    .map(fp.find(x => x.includes('version')))
+    .run()
+    .listen({
+        onRejectd(err) {
+            console.log(err)
+        },
+        onResolved(value) {
+            console.log(value)
+        }
+    })
