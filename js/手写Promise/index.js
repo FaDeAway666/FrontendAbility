886a91c62557cd66e59847f2e1745c000ddbc442
wangxi <136365575@qq.com>
2020-12-01 17:27:05 +0800

    提交 手写promise

--- js/手写Promise/index.js
+++ js/手写Promise/index.js
@@ -8,18 +8,59 @@
  * 4. then方法拥有两个参数，一个是fulfilled回调，一个是reject回调
  *    该方法先判断状态，如果状态是fulfilled，则执行fulfilled回调，如果状态rejected，则执行reject回调
  * 5. then 两个回调的参数，都是相应状态下的返回值
+ * 6. then 方法需要链式调用
+ *    6.1 then方法需要返回promise对象
+ *    6.2 then方法需要接受上一个then方法的返回值，
+ *        是普通值 调用resolve
+ *        是promise对象 查看这个promise的结果，决定调用resolve还是reject
  */
 
- const myPromise = require('./myPromise');
+ const { resolve } = require('./myPromise');
+const MyPromise = require('./myPromise');
+const myPromise = require('./myPromise');
 
  let promise = new myPromise((resolve,reject) => {
-   resolve('success')
+   setTimeout(() => {
+     
+     resolve('success')
+   }, 2000);
+  //  resolve('success')
+  //  throw new Error('error')
 
-   reject('fail')
+  //  reject('fail')
  })
 
- promise.then(value => {
-   console.log(value)
- }, err => {
-   console.log(err)
- })
+ function other() {
+   return new myPromise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('other')
+      }, 2000);
+   })
+ }
+
+let p1 = function() {
+  return new MyPromise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('p1')
+    }, 2000);
+  })
+}
+
+let p2 = function() {
+  return new MyPromise((resolve, reject) => {
+    reject('p2')
+  })
+}
+
+// MyPromise.all(['a','b ',p1(),p2(),'c']).then(res => console.log(res), err => console.log(err))
+
+// MyPromise.race([p1(), p2(), '2']).then(res => console.log(res))
+
+// MyPromise.resolve(100).then(res => console.log(res))
+// MyPromise.resolve(p1()).then(res => console.log(res))
+
+// MyPromise.reject(new Promise((res,rej) => res(111))).then(res => console.log(res)).catch(err => console.log(err))
+
+p2().finally( () => { console.log('finally'); return p1()}).then(res => {console.log(res)},
+  err => {console.log(err)}
+)
--- js/手写Promise/myPromise.js
+++ js/手写Promise/myPromise.js
@@ -4,7 +4,12 @@ const REJECTED = 'rejected'
 
 class MyPromise {
   constructor (executor) {
-    executor(this.resolve, this.reject)
+    try {
+      
+      executor(this.resolve, this.reject)
+    }catch (e) {
+      this.reject(e)
+    }
   }
 
   status = PENDING
@@ -12,11 +17,16 @@ class MyPromise {
   value = undefined
   err = undefined
 
+  onFulfilled = []
+  onRejected = []
+
   resolve = (value) => {
     if(this.status !== PENDING) return
 
     this.status = FULFILLED
     this.value = value
+
+    while(this.onFulfilled.length) this.onFulfilled.shift()()
   }
 
   reject = (err) => {
@@ -24,14 +34,138 @@ class MyPromise {
 
     this.status = REJECTED
     this.err = err
+
+    while(this.onRejected.length) this.onRejected.shift()()
   }
   // then方法定义在原型对象上
   then (onFulfilled, onRejected) {
-    if(this.status === FULFILLED)
-      onFulfilled(this.value)
+    onFulfilled = onFulfilled ? onFulfilled : value => value;
+    onRejected = onRejected ? onRejected : err => new Error(err)
+    let promise = new MyPromise((resolve, reject) => {
 
-    else if(this.status === REJECTED)
-      onRejected(this.err )
+      if(this.status === FULFILLED) {
+        setTimeout(() => {
+          try {
+            let result = onFulfilled(this.value)
+          
+            resolvePromise(promise, result, resolve, reject)
+          }catch (e) {
+            reject(e)
+          }
+            
+        }, 0);
+      }
+  
+      else if(this.status === REJECTED) {
+        setTimeout(() => {
+          try {
+            let result = onRejected(this.err)
+          
+            resolvePromise(promise, result, resolve, reject)
+          }catch (e) {
+            reject(e)
+          }
+            
+        }, 0);
+      }
+         
+      else {
+        this.onFulfilled.push(() => {
+          setTimeout(() => {
+            try {
+              let result = onFulfilled(this.value)
+            
+              resolvePromise(promise, result, resolve, reject)
+            }catch (e) {
+              reject(e)
+            }
+              
+          }, 0);
+        })
+        this.onRejected.push(() => {
+          
+          setTimeout(() => {
+            try {
+              let result = onRejected(this.err)
+            
+              resolvePromise(promise, result, resolve, reject)
+            }catch (e) {
+              reject(e)
+            }
+              
+          }, 0);
+        })
+      }
+    })
+    return promise
+  }
+
+  catch (onRejected) {
+    return this.then(undefined, onRejected)
+  }
+
+  finally (callback) {
+    return this.then(value => {
+      return MyPromise.resolve(callback()).then(() => value )
+    }, err => {
+      return MyPromise.resolve(callback()).then(() => {throw err})
+    })
+      
+  }
+
+  static resolve (value) {
+    if(value instanceof MyPromise) {
+      return value
+    } else {
+      return new MyPromise(resolve => resolve(value))
+    }
+  }
+
+  static reject (err) {
+    return new MyPromise((resolve, reject) => reject(err))
+  }
+
+  static all (array) {
+    let result = []
+    let index = 0
+    return new MyPromise((resolve, reject) => {
+      function addData (i, data) {
+        result[i] = data
+        index++
+        if (index === array.length) resolve(result)
+      }
+      for (let i = 0; i < array.length; i++) {
+        if(array[i] instanceof MyPromise) {
+          array[i].then(value => addData(i, value), err => reject(err))
+        } else {
+          addData(i, array[i])
+        }
+      }
+    })
+  }
+
+  static race (array) {
+    return new MyPromise((resolve, reject) => {
+      for (let i = 0; i < array.length; i++) {
+        if(array[i] instanceof MyPromise) {
+          array[i].then(value => resolve(value), err => reject(err))
+        } else {
+          resolve(array[i])
+        }
+      }
+    })
+  }
+}
+
+function resolvePromise(promise, obj, resolve, reject) {
+  if (obj === promise) {
+    return reject(new TypeError('chain promise error'))
+  }
+
+  if(obj instanceof MyPromise) {
+    obj.then(resolve, reject)
+  } else {
+    resolve(obj)
   }
 }
 
