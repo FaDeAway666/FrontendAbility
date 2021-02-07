aade9f6334d5b6771eb009c8923804356925392f
wangxi <136365575@qq.com>
2021-02-07 09:41:40 +0800

    doc: 增加 api 接口注释

--- framework/vue/vueSSR/realword-nuxt/api/article.js
+++ framework/vue/vueSSR/realword-nuxt/api/article.js
@@ -1,28 +1,33 @@
 // import request from '../utils/request'
 import { request } from '@/plugins/request'
 
+// 获取文章列表
 export const articleList = params => request({
   method: 'GET',
   url: '/api/articles',
   params
 })
 
+// 获取标签
 export const getTags = () => request({
   method: 'GET',
   url: '/api/tags'
 })
 
+// 获取关注的文章列表
 export const feedArticles = params => request({
   method: 'GET',
   url: '/api/articles/feed',
   params
 })
 
+// 点赞
 export const favorite = slug => request({
   method: 'POST',
   url: `/api/articles/${slug}/favorite`
 })
 
+// 取消点赞
 export const cancleFavorite = slug => request({
   method: 'DELETE',
   url: `/api/articles/${slug}/favorite`
